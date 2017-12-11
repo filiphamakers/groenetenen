2fb19efc2f2ac32c418251c5782c20b984d66e3e
VDABHEVERLEE\filip.hamakers <filiphamakers@hotmail.com>
2017-12-11 15:22:16 +0100

    Aanpassingen t.e.m. H22.9

--- src/main/java/be/vdab/valueobjects/PostcodeReeks.java
+++ src/main/java/be/vdab/valueobjects/PostcodeReeks.java
@@ -1,9 +1,17 @@
 package be.vdab.valueobjects;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+import be.vdab.constraints.Postcode;
+
 public class PostcodeReeks {
-	private final static int MIN_POSTCODE = 1000;
-	private final static int MAX_POSTCODE = 9999;
+	@NotNull
+	@Postcode
 	private Integer vanpostcode;
+	@NotNull
+	@Postcode
 	private Integer totpostcode;
 
 	public boolean bevat(int postcode) {
@@ -20,19 +28,11 @@ public class PostcodeReeks {
 	}
 
 	public void setVanpostcode(Integer vanpostcode) {
-		valideer(vanpostcode);
 		this.vanpostcode = vanpostcode;
 	}
 
 	public void setTotpostcode(Integer totpostcode) {
-		valideer(totpostcode);
 		this.totpostcode = totpostcode;
-	}
-
-	private void valideer(int postcode) {
-		if (postcode < MIN_POSTCODE || postcode > MAX_POSTCODE) {
-			throw new IllegalArgumentException();
-		}
 	}
 
 }
