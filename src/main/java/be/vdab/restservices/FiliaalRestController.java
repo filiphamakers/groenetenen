016002e1d537bc262765158625cab474342842c6
DESKTOP-BFJ3OO6\Filip <filiphamakers@hotmail.com>
2017-12-18 16:38:31 +0100

    no message

--- src/main/java/be/vdab/restservices/FiliaalRestController.java
+++ src/main/java/be/vdab/restservices/FiliaalRestController.java
@@ -3,8 +3,14 @@ package be.vdab.restservices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+import be.vdab.services.FiliaalService;
+
+@RestController
 @RequestMapping("/filialen")
 class FiliaalRestController {
+	private final FiliaalService filiaalService;
 
+	FiliaalRestController(FiliaalService filiaalService) {
+		this.filiaalService = filiaalService;
+	}
 }
