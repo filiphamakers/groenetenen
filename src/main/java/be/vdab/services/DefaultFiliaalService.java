8ea0c37f748fa525245763162e9faf8e6b744f1e
DESKTOP-BFJ3OO6\Filip <filiphamakers@hotmail.com>
2017-12-27 16:13:59 +0100

    Aanpassingen t.e.m H38.5 p.148

--- src/main/java/be/vdab/services/DefaultFiliaalService.java
+++ src/main/java/be/vdab/services/DefaultFiliaalService.java
@@ -8,21 +8,25 @@ import org.springframework.data.domain.Sort;
 
 import be.vdab.entities.Filiaal;
 import be.vdab.exceptions.FiliaalHeeftNogWerknemersException;
+import be.vdab.mail.MailSender;
 import be.vdab.repositories.FiliaalRepository;
 import be.vdab.valueobjects.PostcodeReeks;
 
 @ReadOnlyTransactionalService
 public class DefaultFiliaalService implements FiliaalService {
 	private final FiliaalRepository filiaalRepository;
+	private final MailSender mailSender;
 
-	DefaultFiliaalService(FiliaalRepository filiaalRepository) {
+	DefaultFiliaalService(FiliaalRepository filiaalRepository, MailSender mailSender) {
 		this.filiaalRepository = filiaalRepository;
+		this.mailSender = mailSender;
 	}
 
 	@Override
 	@ModifyingTransactionalServiceMethod
 	public void create(Filiaal filiaal) {
 		filiaalRepository.save(filiaal);
+		mailSender.nieuwFiliaalMail(filiaal);
 	}
 
 	@Override
